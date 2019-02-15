5dfbe78d767145ec634121a4223b7d5fd1bfecd8
MatejMaricic <matej.maricic@gmail.com>
2019-02-15 18:43:59 +0100

    First commit, installed packages via composer, created needed entities without any properties yet, index controller, testing index page

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matej
+ * Date: 15.02.19.
+ * Time: 18:06
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+class IndexController extends AbstractController
+{
+    /**
+     * @param Request $request
+     * @Route ("/", name="index_page")
+     * @return Response
+     */
+
+    public function index(Request $request)
+    {
+    return $this->render('index.html.twig');
+    }
+}
