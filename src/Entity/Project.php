9a8edcd27a45f2493be10da2cd3daa1b13476676
MatejMaricic <matej.maricic@gmail.com>
2019-02-16 12:01:44 +0100

    added property for projectmanager class

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -16,8 +16,26 @@ class Project
      */
     private $id;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\ProjectManager", inversedBy="project")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $projectManager;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getProjectManager(): ?ProjectManager
+    {
+        return $this->projectManager;
+    }
+
+    public function setProjectManager(?ProjectManager $projectManager): self
+    {
+        $this->projectManager = $projectManager;
+
+        return $this;
     }
 }
--- src/Entity/ProjectManager.php
+++ src/Entity/ProjectManager.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -16,8 +18,100 @@ class ProjectManager
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdat;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Project", mappedBy="projectManager", orphanRemoval=true)
+     */
+    private $project;
+
+    public function __construct()
+    {
+        $this->project = new ArrayCollection();
+    }
+
     public function getId()
     {
         return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getCreatedat(): ?\DateTimeInterface
+    {
+        return $this->createdat;
+    }
+
+    public function setCreatedat(\DateTimeInterface $createdat): self
+    {
+        $this->createdat = $createdat;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Project[]
+     */
+    public function getProject(): Collection
+    {
+        return $this->project;
+    }
+
+    public function addProject(Project $project): self
+    {
+        if (!$this->project->contains($project)) {
+            $this->project[] = $project;
+            $project->setProjectManager($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProject(Project $project): self
+    {
+        if ($this->project->contains($project)) {
+            $this->project->removeElement($project);
+            // set the owning side to null (unless already changed)
+            if ($project->getProjectManager() === $this) {
+                $project->setProjectManager(null);
+            }
+        }
+
+        return $this;
     }
 }
