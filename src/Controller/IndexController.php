dfc45e9cff0d015da104213cf2dd34e7549e70ef
Your Name <you@example.com>
2019-02-21 19:30:19 +0100

    code reformat

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,21 +8,15 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use App\Entity\Project;
-use App\Entity\ProjectStatus;
-use App\Form\ProjectStatusFormType;
 use App\Form\ProjectFormType;
 use App\Repository\ProjectRepository;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\FormTypeInterface;
-use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 
