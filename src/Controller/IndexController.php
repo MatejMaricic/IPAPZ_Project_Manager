74ac56729603cce0f4188442430e43402afd617d
MatejMaricic <matej.maricic@gmail.com>
2019-02-22 13:24:31 +0100

    made some changes, add developer now on index page and will add assign developer on project page

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -18,57 +18,62 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
-
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Form\RegistrationFormType;
 
 
 class IndexController extends AbstractController
 {
 
 
-    /**
-     * @param Request $request
-     * @Route("/", name="index_page")
-     * @param EntityManagerInterface $entityManager
-     * @param ProjectRepository $projectRepository
-     * @param UserRepository $userRepository
-     * @return Response
-     */
-  public function newProject(Request $request, EntityManagerInterface $entityManager, ProjectRepository $projectRepository, UserRepository $userRepository)
-  {
+    private function newProject(Request $request, EntityManagerInterface $entityManager, $projectForm)
+    {
 
 
+        /** @var Project $project */
 
-      $form = $this->createForm(ProjectFormType::class);
-      $form->handleRequest($request);
-      if ($this->isGranted('ROLE_MANAGER') && $form->isSubmitted() && $form->isValid()) {
-          /** @var Project $project */
-
-          $project = $form->getData();
-          $project->addUser($this->getUser());
-          $entityManager->persist($project);
-
-          $entityManager->flush();
-
-          $this->addFlash('success', 'New project created!');
-          return $this->redirectToRoute('index_page');
-      }
+        $project = $projectForm->getData();
+        $project->addUser($this->getUser());
+        $entityManager->persist($project);
+        $entityManager->flush();
 
 
-      return $this->render('index.html.twig' , [
-          'form' => $form->createView(),
-          'projects' => $projectRepository->findAll(),
-          'user' => $this->getUser(),
-          'users' => $userRepository->findAllDevelopersArray()
+    }
 
-      ]);
-  }
+    private function addDeveloper(Request $request, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoder, $form)
+    {
+        /**@var User $user */
+        $user = $form->getData();
+        $file = $request->files->get('registration_form')['avatar'];
+
+        if (isset($file)) {
+            $uploads_directory = $this->getParameter('uploads_directory');
+            $filename = md5(uniqid()) . '.' . $file->guessExtension();
+            $file->move(
+                $uploads_directory,
+                $filename
+            );
+            $user->setAvatar($filename);
+        }
+
+        $user->setPassword(
+            $passwordEncoder->encodePassword(
+                $user,
+                $form->get('plainPassword')->getData()
+            )
+        );
+        $user->setRoles(array('ROLE_USER'));
+        $entityManager->persist($user);
+        $entityManager->flush();
+    }
+
     /**
      * @Route("/{id}/delete", name="project_delete", methods={"POST", "GET"})
      * @param Project $project
      * @param EntityManagerInterface $entityManager
      * @return JsonResponse
      */
-    public function deleteProject(Project $project, EntityManagerInterface $entityManager )
+    public function deleteProject(Project $project, EntityManagerInterface $entityManager)
     {
 
         $projectId = $project->getId();
@@ -78,16 +83,51 @@ class IndexController extends AbstractController
                 'msg' => 'Unable to delete'
             ]);
         }
-
         $entityManager->remove($project);
         $entityManager->flush();
-
 
         return new JsonResponse([
             'deletedProject' => $projectId
         ]);
     }
 
+    /**
+     * @param Request $request
+     * @Route("/", name="index_page")
+     * @param EntityManagerInterface $entityManager
+     * @param ProjectRepository $projectRepository
+     * @param UserRepository $userRepository
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @return Response
+     */
+    public function indexHandler(Request $request, EntityManagerInterface $entityManager, ProjectRepository $projectRepository, UserRepository $userRepository, UserPasswordEncoderInterface $passwordEncoder)
+    {
+
+        $projectForm = $this->createForm(ProjectFormType::class);
+        $devForm = $this->createForm(RegistrationFormType::class);
+
+        $projectForm->handleRequest($request);
+        if ($this->isGranted('ROLE_MANAGER') && $projectForm->isSubmitted() && $projectForm->isValid()) {
+            $this->newProject($request, $entityManager, $projectForm);
+            $this->addFlash('success', 'New project created!');
+            return $this->redirect($request->getUri());
+        } else {
+            $devForm->handleRequest($request);
+            if ($this->isGranted('ROLE_MANAGER') && $devForm->isSubmitted() && $devForm->isValid()) {
+                $this->addDeveloper($request, $entityManager, $passwordEncoder, $devForm);
+                return $this->redirect($request->getUri());
+            }
+
+            return $this->render('index.html.twig', [
+                'projectForm' => $projectForm->createView(),
+                'projects' => $projectRepository->findAll(),
+                'user' => $this->getUser(),
+                'users' => $userRepository->findAllDevelopersArray(),
+                'form' => $devForm->createView()
+
+            ]);
+        }
 
 
+    }
 }
--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -13,7 +13,6 @@ use App\Entity\Project;
 use App\Entity\User;
 use App\Entity\Task;
 use App\Form\CommentFormType;
-use App\Form\RegistrationFormType;
 use App\Form\TaskFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,35 +24,6 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ProjectController extends AbstractController
 {
-
-    private function addDeveloper(Project $project, Request $request, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoder, $form)
-    {
-        /**@var User $user */
-        $user = $form->getData();
-        $file = $request->files->get('registration_form')['avatar'];
-
-        if (isset($file)) {
-            $uploads_directory = $this->getParameter('uploads_directory');
-            $filename = md5(uniqid()) . '.' . $file->guessExtension();
-            $file->move(
-                $uploads_directory,
-                $filename
-
-            );
-            $user->setAvatar($filename);
-        }
-
-        $user->setPassword(
-            $passwordEncoder->encodePassword(
-                $user,
-                $form->get('plainPassword')->getData()
-            )
-        );
-        $user->setRoles(array('ROLE_USER'));
-        $user->addProject($project);
-        $entityManager->persist($user);
-        $entityManager->flush();
-    }
 
 
     private function addTask(Request $request, EntityManagerInterface $entityManager, Project $project, $taskForm)
@@ -108,27 +78,20 @@ class ProjectController extends AbstractController
      */
     public function projectHandler(Project $project, Request $request, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoder)
     {
-        $form = $this->createForm(RegistrationFormType::class);
+
         $taskForm = $this->createForm(TaskFormType::class, $data = null, array("project_id" => $project->getId()));
 
-        $form->handleRequest($request);
-        if ($this->isGranted('ROLE_MANAGER') && $form->isSubmitted() && $form->isValid()) {
-            $this->addDeveloper($project, $request, $entityManager, $passwordEncoder, $form);
-            return $this->redirect($request->getUri());
-        } else {
+
             $taskForm->handleRequest($request);
             if ($this->isGranted('ROLE_MANAGER') && $taskForm->isSubmitted() && $taskForm->isValid()) {
                 $this->addTask($request, $entityManager, $project, $taskForm);
                 return $this->redirect($request->getUri());
             }
             return $this->render('project/project.html.twig', [
-                'form' => $form->createView(),
                 'taskForm' => $taskForm->createView(),
                 'user' => $this->getUser(),
                 'project' => $project
             ]);
-        }
-
 
     }
 
