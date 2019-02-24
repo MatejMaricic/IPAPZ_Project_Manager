4fcfc6bdc7579c9d4eb55dc78dd401237bb76363
MatejMaricic <matej.maricic@gmail.com>
2019-02-24 17:06:30 +0100

    expand comment image on click

--- public/projects.js
+++ public/projects.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    $(document).on('click','.removeTask', function (e) {
+    $('.removeTask').on('click', function (e) {
         e.preventDefault();
         var link = $(e.currentTarget);
         $.ajax({
@@ -25,18 +25,25 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    $(document).on('change','.js-status-change', function (e) {
+    $('.js-status-change').on('change', function (e) {
         e.preventDefault();
         let link = $(this).find(':selected').data('change_status');
         $.ajax({
             method: 'POST',
             url: link
         }).done(function (data) {
+            console.log(data);
+
             var rowHtml =  $('#task-'+data.taskID).prop('outerHTML');
             $('#task-'+data.taskID).remove();
             $('#js-table-status-'+data.newStatusID ).append(rowHtml);
-            $('#js-table-status-'+data.newStatusID+' option[value='+data.newStatusID+']' ).prop('selected', 'selected');
         })
     });
 });
 
+$(function() {
+    $('.pop').on('click', function() {
+        $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+        $('#imagemodal').modal('show');
+    });
+});
