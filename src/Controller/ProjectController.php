6e4f65c26a825ade9b9316d47807f84cf8654500
Matej Maricic <matej.maricic@gmail.com>
2019-03-12 07:15:44 +0100

    github test

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -26,7 +26,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use App\Form\NewStatusFormType;
 
 class ProjectController extends AbstractController
 {
